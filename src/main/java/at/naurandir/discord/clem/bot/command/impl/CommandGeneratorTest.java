66ee62a7fde1145ecc44c73d8074515e3cfe70e7
Naurandir <Naurandir@users.noreply.github.com>
2022-05-20 21:44:38 +0200

    CLEM: added generator as component to spring framework

--- src/main/java/at/naurandir/discord/clem/bot/command/impl/CommandGeneratorTest.java
+++ src/main/java/at/naurandir/discord/clem/bot/command/impl/CommandGeneratorTest.java
@@ -2,16 +2,20 @@ package at.naurandir.discord.clem.bot.command.impl;
 
 import at.naurandir.discord.clem.bot.command.Command;
 import at.naurandir.discord.clem.bot.command.CommandGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author Naurandir
  */
+
+@Slf4j
+@Component
 public class CommandGeneratorTest implements CommandGenerator {
 
     @Override
     public Command getCommand() {
         return new CommandTest();
     }
-    
 }
