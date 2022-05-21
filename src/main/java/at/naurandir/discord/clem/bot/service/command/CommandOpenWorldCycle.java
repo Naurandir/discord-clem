be0166e8deee60cf07ccaf4cee66435369db4b2d
Naurandir <Naurandir@users.noreply.github.com>
2022-05-21 11:01:37 +0200

    CLEM: added correct datetime formatter

--- src/main/java/at/naurandir/discord/clem/bot/service/command/CommandOpenWorldCycle.java
+++ src/main/java/at/naurandir/discord/clem/bot/service/command/CommandOpenWorldCycle.java
@@ -5,6 +5,7 @@ import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,7 @@ public class CommandOpenWorldCycle implements Command {
     @Value("#{'${discord.clem.channels.worldcycle}'.split(',')}")
     private List<String> interestingChannels;
     
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
     private final String pushCetusChanged = "Cetus cycle changed: {newState} - {minutes}m.";
     private final String worldCycleState = "Cetus: {state} - {minutes}m.";
     
@@ -34,7 +36,7 @@ public class CommandOpenWorldCycle implements Command {
     @Override
     public Mono<Void> handle(MessageCreateEvent event, WarframeState warframeState) {
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime expiry = LocalDateTime.parse(warframeState.getCetusCycle().getExpiry());
+        LocalDateTime expiry = LocalDateTime.parse(warframeState.getCetusCycle().getExpiry(), formatter);
         String cetusDiffMinutes = String.valueOf(now.until(expiry, ChronoUnit.MINUTES));
         
         return event.getMessage().getChannel()
@@ -52,7 +54,7 @@ public class CommandOpenWorldCycle implements Command {
         }
         
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime expiry = LocalDateTime.parse(warframeState.getCetusCycle().getExpiry());
+        LocalDateTime expiry = LocalDateTime.parse(warframeState.getCetusCycle().getExpiry(), formatter);
         String cetusDiffMinutes = String.valueOf(now.until(expiry, ChronoUnit.MINUTES));
         
         for (String channelId : interestingChannels) {
