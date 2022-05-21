7d54057379ffddb7cb76522c04150355e2fb7d31
Naurandir <Naurandir@users.noreply.github.com>
2022-05-21 08:19:51 +0200

    CLEM: testing with push notification to channel

--- src/main/java/at/naurandir/discord/clem/bot/ClemBot.java
+++ src/main/java/at/naurandir/discord/clem/bot/ClemBot.java
@@ -83,9 +83,10 @@ public class ClemBot {
         warframeService.updateWorldState(client);
         log.info("updateStatus: updating current status finished.");
         
+        client.rest().getChannelById(Snowflake.of(977443949352472626L)).createMessage("updated2");
+        
         client.getChannelById(Snowflake.of(977443949352472626L))
-                .filter(channel -> channel.getType().equals(Type.GUILD_TEXT))
-                .flatMap(channel -> ((TextChannel) channel).createMessage("updated"))
-                .then();
+                //.filter(channel -> channel.getType().equals(Type.GUILD_TEXT))
+                .flatMap(channel -> ((TextChannel) channel).createMessage("updated")).subscribe();
     }
 }
