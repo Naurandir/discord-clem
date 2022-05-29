d2abb782b64f6d0a69e5e4342f9b89b960ff3111
Naurandir <Naurandir@users.noreply.github.com>
2022-05-29 17:28:00 +0200

    CLEM: added description for mission output

--- src/main/java/at/naurandir/discord/clem/bot/service/command/RelicDropCommand.java
+++ src/main/java/at/naurandir/discord/clem/bot/service/command/RelicDropCommand.java
@@ -31,6 +31,7 @@ public class RelicDropCommand implements Command {
     private static final String RELIC_SEARCH_DESCRIPTION = "Relics that drop desired item, note some can be vaulted.";
     private static final String MISSION_SEARCH_TITLE = "Missions with Relic Drop for '{item}'";
     
+    private static final String MISSION_DESCRIPTION = "Note that only maximum 20 different mission drops are shown here. For other results specific the input more specific.\n";
     private static final String MISSION_DATA = "***{title}*** - {item} {rotation} ({dropChance}%)\n";
 
     @Override
@@ -112,38 +113,6 @@ public class RelicDropCommand implements Command {
         return missionsWithRelics;
     }
     
-    private EmbedCreateSpec generateEmbedRelics(String item, Map<String, String> relicMessages) {
-        return generateEmbed(RELIC_SEARCH_TITLE.replace("{item}", item), 
-                RELIC_SEARCH_DESCRIPTION, 
-                Color.ORANGE, 
-                "https://static.wikia.nocookie.net/warframe/images/0/0e/VoidProjectionsGoldD.png/revision/latest?cb=20160709035734", 
-                relicMessages);
-    }
-    
-    private EmbedCreateSpec generateEmbedMissions(String item, List<String> missionMessages) {
-        StringBuilder descriptionBuilder = new StringBuilder("");
-        missionMessages.stream().limit(20).forEach(entry -> descriptionBuilder.append(entry));
-        
-        return generateEmbed(MISSION_SEARCH_TITLE.replace("{item}", item),
-                descriptionBuilder.toString(),
-                Color.TAHITI_GOLD,
-                "https://preview.redd.it/99m1fk0q9x8z.jpg?width=640&crop=smart&auto=webp&s=625d5cc7a395ecff11d9b58df20eb949ae92ac7d",
-                Map.of());
-    }
-    
-    private EmbedCreateSpec generateEmbed(String title, String description, Color color, String thumbnail, Map<String, String> fieldData) {         
-        EmbedCreateSpec.Builder embedBuilder = EmbedCreateSpec.builder()
-                .color(color)
-                .title(title)
-                .description(description)
-                .thumbnail(thumbnail)
-                .timestamp(Instant.now());
-        
-        fieldData.entrySet().forEach(entry -> embedBuilder.addField(entry.getKey(), entry.getValue(), true));
-        
-        return embedBuilder.build();
-    }
-
     private Map<String, String> getRelicMessages(List<RelicDTO> relicsWithItem, String item) {
         Map<String, String> relicMessages = new HashMap<>();
         
@@ -205,5 +174,37 @@ public class RelicDropCommand implements Command {
         }
         
         return missionMessages;
+    }
+    
+    private EmbedCreateSpec generateEmbedRelics(String item, Map<String, String> relicMessages) {
+        return generateEmbed(RELIC_SEARCH_TITLE.replace("{item}", item), 
+                RELIC_SEARCH_DESCRIPTION, 
+                Color.ORANGE, 
+                "https://static.wikia.nocookie.net/warframe/images/0/0e/VoidProjectionsGoldD.png/revision/latest?cb=20160709035734", 
+                relicMessages);
+    }
+    
+    private EmbedCreateSpec generateEmbedMissions(String item, List<String> missionMessages) {
+        StringBuilder descriptionBuilder = new StringBuilder(MISSION_DESCRIPTION);
+        missionMessages.stream().limit(20).forEach(entry -> descriptionBuilder.append(entry));
+        
+        return generateEmbed(MISSION_SEARCH_TITLE.replace("{item}", item),
+                descriptionBuilder.toString(),
+                Color.TAHITI_GOLD,
+                "https://preview.redd.it/99m1fk0q9x8z.jpg?width=640&crop=smart&auto=webp&s=625d5cc7a395ecff11d9b58df20eb949ae92ac7d",
+                Map.of());
+    }
+    
+    private EmbedCreateSpec generateEmbed(String title, String description, Color color, String thumbnail, Map<String, String> fieldData) {         
+        EmbedCreateSpec.Builder embedBuilder = EmbedCreateSpec.builder()
+                .color(color)
+                .title(title)
+                .description(description)
+                .thumbnail(thumbnail)
+                .timestamp(Instant.now());
+        
+        fieldData.entrySet().forEach(entry -> embedBuilder.addField(entry.getKey(), entry.getValue(), true));
+        
+        return embedBuilder.build();
     }
 }
