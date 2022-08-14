6885601ec6265ad68eacd08212d7648383f60894
Naurandir <Naurandir@users.noreply.github.com>
2022-08-14 21:06:47 +0200

    CLEM: increased scheduled pool size, logging out duration of scheduled tasks

---
+++ src/main/java/at/naurandir/discord/clem/bot/service/LoggingAspect.java
@@ -0,0 +1,34 @@
+package at.naurandir.discord.clem.bot.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+/**
+ *
+ * @author Naurandir
+ */
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Around("@annotation(org.springframework.scheduling.annotation.Scheduled)")
+    public Object logScheduledExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch watch = new StopWatch();
+        watch.start();
+
+        Object proceed = joinPoint.proceed();
+
+        watch.stop();
+
+        if (watch.getTotalTimeMillis() > 5_000) {
+            log.warn("logScheduledExecutionTime: took [{}]ms to execute task [{}]", joinPoint);
+        }
+
+        return proceed;
+    }
+}
